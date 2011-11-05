14c182a900c345bb64e352dfd1a3770f395ec5fe
Chris Blazek <chris.blazek@gmail.com>
2011-11-05 16:14:02 -0500

    added mysql support, need to parse message to pull alarmpin and insert

--- ADTAlert/rubySerial.rb
+++ ADTAlert/rubySerial.rb
@@ -1,24 +1,44 @@
 require 'rubygems'
 require 'serialport'
+require 'mysql'
 
-Dir.chdir("/dev")
-USB = Dir.glob("tty.usbserial*")
-puts "listening to radio #{USB}"
-puts
+#f = File.new("xbeeDump.txt","w")
+#f = IO.popen("xbeeDump.txt","w")
 
-baud_rate = 9600
-data_bits = 8
-stop_bits = 1
-parity = SerialPort::NONE
-i = 0
-sp = SerialPort.new("#{USB}", baud_rate, data_bits, stop_bits, parity)
+begin
 
-#just read forever need to work on hex parsing
-#while true do
-while i < 178 do
-  i += 1
-  s = sp.getc
-  printf("%i: %c : %s\r\n", i.to_s, s, s.to_s(16)  )
+  Dir.chdir("/dev")
+  USB = Dir.glob("tty.usbserial*")
+  puts "listening to radio #{USB}"
+  puts
+
+  parity = SerialPort::NONE
+  sp = SerialPort.new("#{USB}", 9600, 8, 1, SerialPort::NONE)
+
+  # connect to the MySQL server
+  dbh = Mysql.real_connect("localhost","user", "pass", "db")
+
+  i = 0
+  #just read forever need to work on hex parsing
+  #while true do
+  while i < 178 do
+    i += 1
+    s = sp.getc
+    if s.to_s(16) == '7e' then
+        print "\r\n\r\n"
+#       f.print "\r\n\r\n"
+    end
+
+    printf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16)  )
+#   f.printf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16)  )
+#    dbh.query("INSERT INTO  _TEST(AlarmPIN) VALUES('cnslDOORB')")
+
+#   if s.to_s(16)  == "7e" then print "START\r\n" else print "-" end
+
+  end
+  #f.close_write
+  sp.close
+
+  dbh.close if dbh
+
 end
-
-sp.close
