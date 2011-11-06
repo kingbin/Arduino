9be0654a9db52acf1551dae0d09c50269d758a16
Chris Blazek <chris.blazek@gmail.com>
2011-11-06 08:57:08 -0600

    pulled msgs from states OFF, PINA & PINB, need to debug trash tx

--- ADTAlert/rubySerial.rb
+++ ADTAlert/rubySerial.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'serialport'
 require 'mysql'
 
-#f = File.new("xbeeDump.txt","w")
+f = File.new("xbeeDump.txt","w")
 #f = IO.popen("xbeeDump.txt","w")
 
 begin
@@ -16,7 +16,7 @@ begin
   sp = SerialPort.new("#{USB}", 9600, 8, 1, SerialPort::NONE)
 
   # connect to the MySQL server
-  dbh = Mysql.real_connect("localhost","user", "pass", "db")
+#  dbh = Mysql.real_connect("localhost","user", "pass", "db")
 
   i = 0
   #just read forever need to work on hex parsing
@@ -25,20 +25,25 @@ begin
     i += 1
     s = sp.getc
     if s.to_s(16) == '7e' then
-        print "\r\n\r\n"
-#       f.print "\r\n\r\n"
+      print "\r\n\r\n"
+      f.print "\r\n\r\n"
+      #f.puts "*****************\r\n"
+
     end
 
     printf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16)  )
-#   f.printf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16)  )
+    f.printf("%s", s.to_s(16) )
+
+#    f.printf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16) )
+#    sprintf(" [%i:%c:%s] ", i.to_s, s, s.to_s(16), f  )
 #    dbh.query("INSERT INTO  _TEST(AlarmPIN) VALUES('cnslDOORB')")
 
 #   if s.to_s(16)  == "7e" then print "START\r\n" else print "-" end
 
   end
-  #f.close_write
+  f.close_write
   sp.close
 
-  dbh.close if dbh
+#  dbh.close if dbh
 
 end
